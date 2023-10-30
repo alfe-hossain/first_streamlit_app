685d8878f01097a8f259311614fe9deb715be025
alfe-hossain <149413177+alfe-hossain@users.noreply.github.com>
2023-10-30 13:57:05 -0400

    comments

--- streamlit_app.py
+++ streamlit_app.py
@@ -27,7 +27,8 @@ streamlit.header("Fruityvice Fruit Advice!")
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 streamlit.text(fruityvice_response.json())
 
-# write your own comment -what does the next line do? 
+# normalizes the response of the data
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-# write your own comment - what does this do?
+# puts the data in a dataframe/table
+streamlit.text(fruityvice_normalized)
 streamlit.dataframe(fruityvice_normalized)
