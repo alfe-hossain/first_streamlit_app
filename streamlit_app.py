9e29f592705bdf57fbc94c7519e2af9a7dae17c6
alfe-hossain <149413177+alfe-hossain@users.noreply.github.com>
2023-10-30 13:58:20 -0400

    changing to kiwi

--- streamlit_app.py
+++ streamlit_app.py
@@ -24,11 +24,9 @@ streamlit.dataframe(fruits_to_show)
 
 streamlit.header("Fruityvice Fruit Advice!")
 
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response.json())
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + "kiwi)
 
-# normalizes the response of the data
+# extracts the data from the json object
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 # puts the data in a dataframe/table
-streamlit.text(fruityvice_normalized)
 streamlit.dataframe(fruityvice_normalized)
